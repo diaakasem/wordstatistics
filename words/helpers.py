a8c6fc5ecb3f32d371c6f668b597d5df81052e76
ibrahimahmed443 <ibrahimahmed443@gmail.com>
2014-10-17 21:17:19 +0500

    add support for third-level categories in the visualization tree of a dictionary (this code would need to be generalized for n level of categories at some point); add bunch of comments and make code more readable

--- words/helpers.py
+++ words/helpers.py
@@ -1,6 +1,7 @@
 import json
 import copy
 
+import stats
 
 def get_value(d, l):
 	if len(l) > 1:
@@ -33,7 +34,7 @@ def process_visualization(structure):
 
 	#find unique level2 categories ...
 	level2 = [item.keys()[0] for item in data]
-	level2 = set(level2)
+	level2 = set(level2)			#level2 something like: set([501, 502, 504])
 
 	#append level2 to main category...
 	# d = {}
@@ -41,24 +42,75 @@ def process_visualization(structure):
 	# for item in level2:
 	# 	d[top_level_cat].append(item)
 
-	result = {}
+	
 	# d['name'] = top_level_cat
 	# d['children'] = []
 	# for item in level2:
 	# 	d['children'].append({
 	# 			'name': item
 	# 		})
+	
+	
+	#loop through level2 categories...
+	obj = []
+	for item in level2:
+	
+		level3 = []
+		level3_words = []
+
+		#foreach category in level2, find the categories that appear after it (i.e, third level categories)...
+		for word in structure['words']:
+
+			d = {}
+			categories = structure['words'][word]['categories']
+			second_level_cat = categories[1]
+			
+			if second_level_cat != str(item):	#if its not the category we're looking for, skip...
+				continue
+			
+			#check if we have further categories beyond the second for this word;
+			#if yes, save the category, else record the word itself...
+			if len(categories) > 2:
+				level3.append(categories[2])		#categories[2] is a third-level category...
+			else:
+				level3_words.append(word)
+		
+
+		#find unique third level elements...
+		level3 = set(level3)
+		
+		#add all children of second-level categories, note that some children are categories themselves while
+		#some are words; hence the if condition...
+
+		children = []
+		if len(level3) > 0:					#level3 is a category in itself...
+			children.extend([{'name': convert_category_number_to_name(structure, item_level3)} for item_level3 in level3])
+
+		if len(level3_words) > 0:			#level3 is just a word...
+			children.extend([{'name': level3_word} for level3_word in level3_words])
+
+		#create the obj for this second-level category, something like:
+		# {'name': 'HonGain', 'children': [{'name': 'Achieve'}, {'name': 'Status'}]
+
+		obj.append({
+				'name': convert_category_number_to_name(structure, item),
+				'children': children
+			})
+
 
 	result = []
 	result.append({
 		'name': convert_category_number_to_name(structure, top_level_cat),
-		'children': [{"name": convert_category_number_to_name(structure, item)} for item in level2]
-		})
-
+		'children': obj
+	})
+	
 	return result
 
+if __name__ == "__main__":
+	
+	f = open('../uploads/95523d21-1526-4487-b49a-ab7b1a4704f8_Honor_English__v2013Bayan_fixed999.txt')
+	wordsText = f.read()
+	f.close()
 
-
-
-
-
+	structure = stats.buildWordsStructure(wordsText)  
+	process_visualization(structure)
